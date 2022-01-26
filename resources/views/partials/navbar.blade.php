1bbc878a0d4f8a979a55f9222dd64c05968d1b6b
machiattoo <bimanyunugrohofirmansah@gmail.com>
2022-01-26 10:43:19 +0700

    Pengaturan awal tampilan

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+        <a class="navbar-brand" href="#"><i class="bi bi-app"></i> ECM</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-link active" href="/">Home</a>
+            </div>
+            <div class="navbar-nav ms-auto">
+                <a href="/login" class="nav-link active"><i class="bi bi-box-arrow-right"></i> Login</a>
+            </div>
+        </div>
+    </div>
+</nav>
